93296407e25387d7acac4f829439f83cc8fb5649
Wouter <wouter.deloof@gmail.com>
2018-08-09 10:01:14 +0200

    filter script is working, made required adaptations in css for representation of items in grid

---
+++ src/js/filter.js
@@ -0,0 +1,42 @@
+// Engagements filter
+const buttons = Array.prototype.slice.call(
+  document.querySelectorAll(".engagements__btn")
+);
+
+const engagements = Array.prototype.slice.call(
+  document.querySelectorAll(".engagements__item")
+);
+const activeClass = "engagements__btn--active";
+
+const filterEngagements = (tag, isActive) => {
+  if (isActive) {
+    engagements.forEach(engagement => {
+      engagement.style.cssText = "";
+    });
+    document.querySelector(`.${activeClass}`).classList.remove(activeClass);
+  } else {
+    engagements.forEach(engagement => {
+      engagement.style.display = "block";
+      if (engagement.dataset.tag.indexOf(tag) > -1) {
+        engagement.style.display = "none";
+      }
+    });
+  }
+};
+
+const setBtnActive = () => {};
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", e => {
+    setBtnActive();
+    // const activeBtn = document.querySelector(`.${activeClass}`);
+    const isTargetActive =
+      Array.prototype.slice.call(e.target.classList).indexOf(activeClass) > -1;
+
+    // if (activeBtn) {
+    //   activeBtn.classList.remove(activeClass);
+    // }
+    e.target.classList.toggle(activeClass);
+    filterEngagements(e.target.dataset.tag, isTargetActive);
+  });
+});
